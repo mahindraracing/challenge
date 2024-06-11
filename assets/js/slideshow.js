c0af7421827699dc58d91472f256c0a3db13bfcf
imneli <contatodoneli@gmail.com>
2024-06-11 00:43:47 -0300

    Chat - Live

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -16,5 +16,5 @@ function showSlides() {
   }  
   
   slides[slideIndex - 1].style.display = "block";  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 7000); 
 }
